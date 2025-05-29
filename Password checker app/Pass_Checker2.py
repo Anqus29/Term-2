7f9f57e6ccbc6ab83cce6ad857f4581f96ed6ab5
Angus <ABriscoe@student.saintaug.nsw.edu.au>
2025-05-29 12:54:35 +1000

    added saving and other functions

--- Password checker app/Pass_Checker2.py
+++ Password checker app/Pass_Checker2.py
@@ -1,45 +1,163 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import re
-from pynput.keyboard import Key, Listener
 
 class PasswordChecker(ttk.Frame):
     def __init__(self, master=None):
 
         super().__init__(master)
-        self.pack()
+        self.pack(fill='both', expand=True) 
         self.create_widgets()
 
     def create_widgets(self):
 
+
+        # Title frame
+        self.title_bar = ttk.Frame(
+            self, 
+            bootstyle='success', 
+            height=60
+            )
+        self.title_bar.pack(
+            side=TOP, 
+            fill='x'
+            )
+        self.title_bar.pack_propagate(False)
         # Title
-        self.title = ttk.Label(self, text="Password Checker", font=("Arial", 20), bootstyle=SUCCESS)
-        self.title.pack(side=TOP, padx=5, pady=20)
+        self.title = ttk.Label(
+            self.title_bar,
+            text="Password Checker",
+            font=("Arial", 20),
+            bootstyle="inverse-success"
+            )
+        self.title.pack(
+            side=TOP, 
+            pady=10
+            )
 
         # Invisable frame for spacing
-        self.invisible_frame = ttk.Frame(self, bootstyle="success", borderwidth=0, relief="flat")
-        self.invisible_frame.pack(side=TOP, fill='none', padx=0, pady=60)
+        self.invisible_frame = ttk.Frame(
+            self, 
+            borderwidth=0, 
+            relief="flat"
+            )
+        self.invisible_frame.pack(
+            side=TOP,
+            fill='x', 
+            padx=0, 
+            pady=60
+            )
 
         # Password entry text
-        self.instruction = ttk.Label(self, text="Enter your password:", font=("Arial", 14))
-        self.instruction.pack(side=TOP, padx=20, pady=15)
+        self.instruction = ttk.Label(
+            self, 
+            text="Enter your password:", 
+            font=("Arial", 14)
+            )
+        self.instruction.pack(
+            side=TOP, 
+            padx=20, 
+            pady=15
+            )
 
         # Password entry field
-        self.password_entry = ttk.Entry(self, show="*", font=("Arial", 14), bootstyle="success")
-        self.password_entry.pack(side=TOP, padx=20, pady=10)
+        self.password_entry = ttk.Entry(
+            self, 
+            show="*", 
+            font=("Arial", 14), 
+            bootstyle="success"
+            )
+        self.password_entry.pack(
+            side=TOP, 
+            padx=20, 
+            pady=10
+            )
+
+        # Button frame
+        self.button_frame = ttk.Frame(self)
+        self.button_frame.pack(side=TOP, 
+            pady=10
+            )
 
         # Check password button
-        self.check_button = ttk.Button(self, text="Check Password", command=self.check_password, bootstyle="success-OUTLINE")
-        self.check_button.pack(side=TOP, padx=20, pady=10)
-        self.password_entry.bind("<Return>", lambda event: self.check_password())
+        self.check_button = ttk.Button(
+            self.button_frame, 
+            text="Check Password", 
+            command=self.check_password, 
+            bootstyle="success-OUTLINE"
+            )
+        self.check_button.pack(
+            side=LEFT, 
+            padx=5
+            )
+        
+        self.password_entry.bind("<Return>", 
+            lambda event: self.check_password()
+            )
+        
+        # Create password button
+        self.create_password_button = ttk.Button(
+            self.button_frame, 
+            text="Generate Password", 
+            command=self.generate_password, 
+            bootstyle="success-OUTLINE"
+            )
+        self.create_password_button.pack(
+            side=RIGHT, 
+            padx=5
+            )
+        
+        # Save password button
+        self.save_password_button = ttk.Button(
+            self,
+            text="Save Password to Clipboard", 
+            command=self.save_password, 
+            bootstyle="success-OUTLINE"
+            )
+        self.save_password_button.pack(
+            side=TOP, 
+            padx=5,
+            pady=10
+            )
+
+        self.password_length = ttk.Scale(
+            self.button_frame, 
+            from_=8, 
+            to=20, 
+            orient=HORIZONTAL, 
+            bootstyle="success"
+            )
+        self.password_length.pack(
+            side=TOP, 
+            padx=5, 
+            pady=10, 
+            )
 
         # Result percentage
-        self.result_slider = ttk.Progressbar(self, orient=HORIZONTAL, length=300, bootstyle="success")
-        self.result_slider.pack(side=TOP, padx=20, pady=50)
+        self.result_slider = ttk.Progressbar(
+            self, 
+            orient=HORIZONTAL, 
+            length=300, 
+            bootstyle="success"
+            )
+        self.result_slider.pack(
+            side=TOP, 
+            padx=20, 
+            pady=50
+            )
 
         # Password issues
-        self.password_issues_label = ttk.Label(self, text="", font=("Arial", 14), bootstyle="DANGER")
-        self.password_issues_label.pack(side=TOP, padx=20, pady=10)
+        self.password_issues_label = ttk.Label(
+            self, 
+            text="", 
+            font=("Arial", 14), 
+            bootstyle="DANGER"
+            )
+        self.password_issues_label.pack(
+            side=TOP, 
+            padx=20, 
+            pady=10
+            )
 
     def check_password(self):
         password = self.password_entry.get()
@@ -62,11 +180,25 @@ class PasswordChecker(ttk.Frame):
 
         if re.search(r"[{}<>]", password):
             password_issues.append("For security reasons password must not contain any of the following characters: {}<>")
-        self.result_slider['value'] = password_score * 10
+        self.result_slider['value'] = (password_score / 11) * 100
         if password_issues:
             self.password_issues_label.config(text="\n".join(password_issues))
         else:
             self.password_issues_label.config(text="No issues found. Password is strong!")
+    
+    def generate_password(self):
+
+        self.password_entry.delete(0, 'end')
+        
+        self.password_entry.insert(0, "GeneratedPassword123!")
+        self.check_password()
+
+    def save_password(self):
+        password = self.password_entry.get()
+        if password:
+            self.master.clipboard_clear()
+            self.master.clipboard_append(password)
+            self.master.update()
 
 if __name__ == "__main__":
     app = ttk.Window(themename="superhero", title="Password Checker", size=(1080, 720))
